e097179a1fab13467beededb22f85c2c816274be
Casper Smet <casper.smet@student.hu.nl>
2020-02-21 11:42:42 +0100

    Multithreaded merge sort

--- 3_threaded_merge.py
+++ 3_threaded_merge.py
@@ -1,18 +1,36 @@
 import numpy as np
 import cProfile
+import time
 
-# Based on https://medium.com/karuna-sehgal/a-simplified-explanation-of-merge-sort-77089fe03bb2
+import threading
 
-    
+# Based on https://medium.com/karuna-sehgal/a-simplified-explanation-of-recursive_merge-sort-77089fe03bb2
+class sorterThead(threading.Thread):
+    def __init__(self, args):
+        threading.Thread.__init__(self, args=args)
 
-def merge_sort(sub_array: np.array):
+        self.array = args[0]
+
+    def run(self):
+        self.array = recursive_merge_sort(self.array)
+
+    def get(self):
+        return self.array
+
+
+def recursive_merge_sort(sub_array: np.array):
     if sub_array.size <= 1:
         return sub_array
 
-
     left, right = np.array_split(sub_array, 2)
 
-    return merge(merge_sort(left), merge_sort(right))
+    return merge(
+                recursive_merge_sort(left), 
+                recursive_merge_sort(right)
+                )
+
+
+    
 
 def merge(left: np.array, right: np.array):
     index_left   = 0
@@ -38,16 +56,36 @@ def merge(left: np.array, right: np.array):
     return np.concatenate((result[:index_result], left[index_left:], right[index_right:]))
 
 
+def threaded_sort(array: np.array, max_threads=4):
+    threads = []
+    for sub_array in np.array_split(array, min(max_threads, array.size)):
+        new_thread = sorterThead([sub_array])
+        threads.append(new_thread)
+        new_thread.start()
 
+    result_array = threads[0].get()
+    for thread in threads[1:]:
+        thread.join()
+        result_array = merge(result_array, thread.get())
+
+    return result_array
+        
 def main():
-    array = np.array([2, 5, 1, 3, 7, 4, 2, 3, 9, 8, 6, 3])
-    print(array)
-    pr = cProfile.Profile()
-    pr.enable()
-    sorted_array = merge_sort(array)
-    pr.disable()
-    print(sorted_array)
-    pr.print_stats()
+    # array = np.array([2, 5, 1, 3, 7, 4, 2, 3, 9, 8, 6, 3])
+    array = np.random.randint(0, 100, 1000)
+    
+    reg1 = time.time()
+    sorted_array = recursive_merge_sort(array)
+    reg2 = time.time()
+
+
+    thr1 = time.time()
+    thread_sorted_array = threaded_sort(array)
+    thr2 = time.time()
+
+
+    print(reg2-reg1, thr2 - thr1)
+
 
 if __name__ == "__main__":
     main()
